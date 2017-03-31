13d5394d7e7aded8d79a891f5e6f0775a5bc7909
cicime <yuanming200755@163.com>
2017-03-31 12:11:23 +0800

    :sunrise_over_mountains: :kissing_smiling_eyes:

---
+++ src/components/Works.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="vux-demo">
+      <img class="logo" src="../assets/vux_logo.png">
+      <h1> </h1>
+    </div>
+    <group title="cell demo">
+      <cell title="Vux" value="Cool" is-link> works </cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import { Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell
+  },
+  mounted() {
+
+  },
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/HelloFromVux'
 import Home from '@/components/Home'
+import Works from '@/components/Works'
+import Articles from '@/components/Articles'
+import User from '@/components/User'
 
 Vue.use(Router)
 
@@ -9,6 +12,26 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/works',
+      name: 'Works',
+      component: Works
+    },
+    {
+      path: '/articles',
+      name: 'Articles',
+      component: Articles
+    },
+    {
+      path: '/user',
+      name: 'User',
+      component: User
+    },
+    {
+      path: '*',
       name: 'Home',
       component: Home
     }
